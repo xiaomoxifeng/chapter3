5c163b71657f0b72aa61b37bda37f10ac3a91090
jsonwu <jsonwu@raventech.com>
2016-03-29 12:28:30 +0800

    init

---
+++ src/main/java/org/smart4j/chapter3/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package org.smart4j.chapter3.controller;
+
+import com.json.smart4j.framework.annotation.Action;
+import com.json.smart4j.framework.annotation.Controller;
+import com.json.smart4j.framework.annotation.Inject;
+import com.json.smart4j.framework.bean.Param;
+import com.json.smart4j.framework.bean.View;
+import org.smart4j.chapter3.model.Customer;
+import org.smart4j.chapter3.service.CustomerService;
+
+import java.util.List;
+
+/**
+ * Created by wuhao on 16/3/24.
+ */
+@Controller
+public class CustomerController {
+@Inject
+private CustomerService customerService;
+    /**
+     * 进入 客户列表 界面
+     */
+    @Action("get:/customer")
+    public View index(Param param) {
+        List<Customer> customerList = customerService.getCustomerList();
+        return new View("customer.jsp").addModel("customerList", customerList);
+    }
+}
---
+++ src/main/java/org/smart4j/chapter3/service/CustomerService.java
@@ -0,0 +1,51 @@
+package org.smart4j.chapter3.service;
+
+import com.json.smart4j.framework.annotation.Service;
+import com.json.smart4j.framework.helper.DatabaseHelper;
+import org.smart4j.chapter3.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wuhao on 16/3/24.
+ */
+@Service
+public class CustomerService {
+    /**
+     * 获取客户列表
+     */
+    public List<Customer> getCustomerList() {
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
+    }
+
+    /**
+     * 获取客户
+     */
+    public Customer getCustomer(long id) {
+        String sql = "SELECT * FROM customer WHERE id = ?";
+        return DatabaseHelper.queryEntity(Customer.class, sql, id);
+    }
+
+    /**
+     * 创建客户
+     */
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
+    }
+
+    /**
+     * 更新客户
+     */
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
+    }
+
+    /**
+     * 删除客户
+     */
+    public boolean deleteCustomer(long id) {
+        return DatabaseHelper.deleteEntity(Customer.class, id);
+    }
+}
